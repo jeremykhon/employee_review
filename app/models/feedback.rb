851872904a8c3617360ba938794a2678d7838dce
Jeremy Hon <jehon11@gmail.com>
2019-06-23 18:38:33 +0900

    update validations

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,6 @@
 class Feedback < ApplicationRecord
   belongs_to :employee
   belongs_to :performance_review
+
+  validates :employee, :performance_review, :comment, :rating, presence: true
 end
--- app/models/performance_review.rb
+++ app/models/performance_review.rb
@@ -2,4 +2,6 @@ class PerformanceReview < ApplicationRecord
   belongs_to :employee
   has_many :feedbacks
   has_many :reviewers, through: :feedbacks, source: :employee
+
+  validates :employee, presence: true
 end
