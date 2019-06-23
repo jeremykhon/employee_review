1845ecc2a62aaa1d7aa7bb48e79ead39679bad77
Jeremy Hon <jehon11@gmail.com>
2019-06-23 20:12:30 +0900

    implement CRUD actions for employees

---
+++ app/controllers/api/v1/employees_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::EmployeesController < ApplicationController
+  before_action :set_employee, only: %i[show destroy update]
+
+  def index
+    employees = Employee.all
+    render json: employees, only: %i[id first_name last_name email]
+  end
+
+  def show
+    render json: @employee, only: %i[id first_name last_name email]
+  end
+
+  def create
+    employee = Employee.create!(employee_params)
+    render json: employee, only: %i[id first_name last_name email]
+  end
+
+  def destroy
+    @employee.destroy!
+    head :ok
+  end
+
+  def update
+    @employee.update!(employee_params)
+    head :ok
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find_by!(id: params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :password)
+  end
+end
