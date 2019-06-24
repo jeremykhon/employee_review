42e5efb38f8fba28b88072607096b734bb3bef8a
Jeremy Hon <jehon11@gmail.com>
2019-06-24 12:25:24 +0900

    update feedback model validations

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,5 +2,5 @@ class Feedback < ApplicationRecord
   belongs_to :employee
   belongs_to :performance_review
 
-  validates :employee, :performance_review, :comment, :rating, presence: true
+  validates :employee, :performance_review, presence: true
 end
