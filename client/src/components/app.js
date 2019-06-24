5a3a8537c518360cf0c3938c84bf9d5b75896add
Jeremy Hon <jehon11@gmail.com>
2019-06-24 17:29:19 +0900

    create router and routes

---
+++ client/src/components/app.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from '../utilities/history';
+import Navbar from './navbar';
+import LoginPage from './login_page';
+import AdminDashboard from './admin_dashboard';
+import EmployeeDashboard from './employee_dashboard';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      employee: null,
+    }
+  }
+
+  render() {
+    return (
+      <Router history={history}>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/admin" component={AdminDashboard} />
+          <Route exact path="/employee" component={EmployeeDashboard} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
