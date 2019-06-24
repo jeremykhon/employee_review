0f2513d5327b9d2510e69c18d60c70e22e5b7805
Jeremy Hon <jehon11@gmail.com>
2019-06-25 00:13:15 +0900

    fetch employee when selected employee is updated

--- client/src/components/employee_profile.js
+++ client/src/components/employee_profile.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import BASE_URL from '../utilities/base_url';
 
-const EmployeeProfile = ({ selectedEmployeeId }) => {
-  return (
-    <div className="employee-profile">
-      {selectedEmployeeId}
-    </div>
-  );
-};
+class EmployeeProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employee: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchEmployee();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.selectedEmployeeId !== prevProps.selectedEmployeeId) {
+      this.fetchEmployee(this.props.selectedEmployeeId);
+    }
+  }
+
+  fetchEmployee = (employeeId) => {
+    axios.get(`${BASE_URL}/employees/${employeeId}`)
+      .then(response => this.setState({ employee: response.data }))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    const { selectedEmployeeId } = this.props;
+    const { employee } = this.state;
+    if (employee) {
+      return (
+        <div className="employee-profile">
+          <div>
+            {selectedEmployeeId}
+          </div>
+          <div>
+            {employee.first_name}
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="employee-profile">
+        nothing
+      </div>
+    );
+  }
+}
 
 export default EmployeeProfile;
