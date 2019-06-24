93a0c83f44795572e9d00c598ff83ab6f113ce58
Jeremy Hon <jehon11@gmail.com>
2019-06-25 02:45:50 +0900

    add dependent destroy on employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,9 +1,9 @@
 class Employee < ApplicationRecord
   has_secure_password
 
-  has_many :feedbacks
+  has_many :feedbacks, dependent: :destroy
   has_many :performance_reviews_to_review, through: :feedbacks, source: :performance_review
-  has_many :performance_reviews
+  has_many :performance_reviews, dependent: :destroy
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
