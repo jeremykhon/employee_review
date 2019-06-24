ad86df62225005f1046fbcc23594c4c3024d73a6
Jeremy Hon <jehon11@gmail.com>
2019-06-24 12:09:21 +0900

    configure routes for employees, performance reviews, and feedbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :employees
+      resources :employees do
+        resources :feedbacks, only: :index
+        resources :performance_reviews, only: %i[index create] do
+        end
+      end
+      resources :performance_reviews, only: :update do
+        resources :feedbacks, only: :create
+        get 'feedbacks', to: 'feedbacks#index_by_performance_review'
+      end
+      resources :feedbacks, only: :update
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
