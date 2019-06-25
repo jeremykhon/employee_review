19ddd11500012bc6953cd15191d165f00ec8fffc
Jeremy Hon <jehon11@gmail.com>
2019-06-25 14:36:00 +0900

    can fetch feedbacks per employee performance review

--- client/src/components/performance_review.js
+++ client/src/components/performance_review.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import BASE_URL from '../utilities/base_url';
 
 class PerformanceReview extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      something: null,
+      feedbacks: [],
     };
+  }
+
+  componentDidMount() {
+    this.fetchFeedbacksPerPerformanceReview();
+  }
+
+  fetchFeedbacksPerPerformanceReview = () => {
+    const { performanceReview } = this.props;
+    axios.get(`${BASE_URL}/performance_reviews/${performanceReview.id}/feedbacks`)
+      .then(response => this.setState({ feedbacks: response.data }))
+      .catch(error => console.log(error));
+  }
+
+  renderFeedbacks = () => {
+    const { feedbacks } = this.state;
+    if (feedbacks.length > 0) {
+      return (
+        feedbacks.map(feedback => <div>{feedback.employee_id}</div>)
+      );
+    }
+    return null;
   }
 
   render() {
     const { performanceReview } = this.props;
     return (
       <div className="performance-review">
-        {performanceReview.title}
+        <div>{performanceReview.title}</div>
+        <div>
+          {this.renderFeedbacks()}
+        </div>
+        
       </div>
     );
   }
