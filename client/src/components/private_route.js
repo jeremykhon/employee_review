6051f1007b0506a38bea28634c0464c04cf7c5d6
Jeremy Hon <jehon11@gmail.com>
2019-06-26 03:13:46 +0900

    create private route

---
+++ client/src/components/private_route.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, adminOnly, employee, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (employee) {
+          if (employee.is_admin) {
+            return <Component {...props} />;
+          }
+          if (!adminOnly) {
+            return <Component {...props} />;
+          }
+          return <Redirect to={{ pathname: '/', state: { from: props.location } }} />;
+        }
+
+        return <Redirect to={{ pathname: '/', state: { from: props.location } }} />;
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
