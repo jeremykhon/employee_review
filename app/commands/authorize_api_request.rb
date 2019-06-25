309d9f158545872a44889f418f1c67a6c8457baa
Jeremy Hon <jehon11@gmail.com>
2019-06-25 21:45:54 +0900

    fix typo in authorize api request

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -14,7 +14,7 @@ class AuthorizeApiRequest
   attr_reader :headers
 
   def employee
-    @employee ||= employee.find(decoded_auth_token[:employee_id]) if decoded_auth_token
+    @employee ||= Employee.find(decoded_auth_token[:employee_id]) if decoded_auth_token
     @employee || errors.add(:token, 'Invalid token') && nil
   end
 
