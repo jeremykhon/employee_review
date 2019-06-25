22d4b2d4ae00d0bee8db6fd21aee41fa893d4275
Jeremy Hon <jehon11@gmail.com>
2019-06-25 21:30:19 +0900

    create authenticate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      post 'authenticate', to: 'authentication#authenticate'
       resources :employees do
         resources :feedbacks, only: :index
         resources :performance_reviews, only: %i[index create] do
