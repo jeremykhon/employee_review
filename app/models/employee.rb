de5f7c9c7709fe7e94447cc402f98a567dcd6491
Jeremy Hon <jehon11@gmail.com>
2019-06-26 21:29:03 +0900

    order employees feedbacks by id

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,8 @@
 class Employee < ApplicationRecord
   has_secure_password
 
-  has_many :feedbacks, dependent: :destroy
+  has_many :feedbacks, -> { order('id DESC') }, dependent: :destroy
+
   has_many :performance_reviews_to_review, through: :feedbacks, source: :performance_review
   has_many :performance_reviews, dependent: :destroy
 
