b8096150ca7c9bf91aa18ed9d22876f4b21c96c7
Jeremy Hon <jehon11@gmail.com>
2019-06-26 17:55:55 +0900

    move selected style method into employee list item component

--- client/src/components/employee_list_item.js
+++ client/src/components/employee_list_item.js
@@ -1,24 +1,20 @@
 import React from 'react';
+import classNames from 'classnames';
 import history from '../lib/history';
 
 const selectEmployee = (employeeId) => {
   history.push(`/admin/employees/${employeeId}`);
 };
 
-const selectedStyle = (employee, selectedEmployeeId) => {
-  if (employee.id === parseInt(selectedEmployeeId, 10)) {
-    return 'employee-list-item selected';
-  }
-  return 'employee-list-item';
-};
-
 const EmployeeListItem = ({ employee, selectedEmployeeId }) => {
+  const employeeSelected = (employee.id === parseInt(selectedEmployeeId, 10));
+
   return (
-    <div 
-      className={selectedStyle(employee, selectedEmployeeId)} 
+    <div
+      className={classNames('employee-list-item', { selected: employeeSelected })}
       onClick={() => {selectEmployee(employee.id)}}
     >
-      {`${employee.first_name} ${employee.last_name}`} 
+      {`${employee.first_name} ${employee.last_name}`}
     </div>
   );
 };
