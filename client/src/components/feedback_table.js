89e88a396a80a11cb693840eb257f89aeac7384c
Jeremy Hon <jehon11@gmail.com>
2019-06-26 21:30:22 +0900

    config table timestamps and column max width

--- client/src/components/feedback_table.js
+++ client/src/components/feedback_table.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import moment from 'moment';
 
 const FeedbackRow = ({ feedback }) => {
   return (
     <tr>
-      <td>{feedback.id}</td>
       <td>{feedback.employee.first_name}</td>
       <td>{feedback.employee.last_name}</td>
-      <td>{feedback.comment}</td>
+      <td className="comment-cell">{feedback.comment}</td>
       <td>{feedback.rating}</td>
-      <td>{feedback.completed_at}</td>
+      <td>{feedback.completed_at ? moment(feedback.completed_at).format('MMMM Do YYYY') : null}</td>
     </tr>
   );
 };
@@ -19,10 +19,9 @@ const FeedbackTable = ({ feedbacks }) => {
       <table className="table feedback-table">
         <thead>
           <tr>
-            <th>ID</th>
             <th>First name</th>
             <th>Last name</th>
-            <th>Comment</th>
+            <th className="comment-cell">Comment</th>
             <th>Rating</th>
             <th>Completed</th>
           </tr>
--- client/src/components/feedback_todo_table.js
+++ client/src/components/feedback_todo_table.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import moment from 'moment';
+
+const compareFeedback = (feedback, selectedFeedback) => {
+  if (selectedFeedback) {
+    if (feedback.id === selectedFeedback.id) return true;
+  }
+  return false;
+};
 
 const FeedbackTodoRow = ({ feedback, selectFeedback, selectedFeedback }) => {
   return (
-    <tr onClick={() => selectFeedback(feedback)}>
-      <td>{feedback.id}</td>
+    <tr className={compareFeedback(feedback, selectedFeedback) ? 'selected table-row' : 'table-row'} onClick={() => selectFeedback(feedback)}>
       <td>{feedback.performance_review.employee.first_name}</td>
       <td>{feedback.performance_review.employee.last_name}</td>
-      <td>{feedback.comment}</td>
+      <td className="comment-cell">{feedback.comment}</td>
       <td>{feedback.rating}</td>
-      <td>{feedback.completed_at}</td>
+      <td>{feedback.completed_at ? moment(feedback.completed_at).format('MMMM Do YYYY') : null}</td>
     </tr>
   );
 };
@@ -19,10 +26,9 @@ const FeedbackTodoTable = ({ feedbacks, selectFeedback, selectedFeedback }) => (
       <table className="table feedback-table">
         <thead>
           <tr>
-            <th>ID</th>
             <th>First name</th>
             <th>Last name</th>
-            <th>Comment</th>
+            <th className="comment-cell">Comment</th>
             <th>Rating</th>
             <th>Completed</th>
           </tr>
