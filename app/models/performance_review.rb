20a465e3f46bd298145afa5202536bc8e16ec6f1
Jeremy Hon <jehon11@gmail.com>
2019-06-26 21:28:47 +0900

    performance review can be deleted even if have feedbacks

--- app/models/performance_review.rb
+++ app/models/performance_review.rb
@@ -1,6 +1,6 @@
 class PerformanceReview < ApplicationRecord
   belongs_to :employee
-  has_many :feedbacks
+  has_many :feedbacks, dependent: :destroy
   has_many :reviewers, through: :feedbacks, source: :employee
 
   validates :employee, presence: true
