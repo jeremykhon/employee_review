133a620013164274d64c9bb98169b76ccdaf65de
Jeremy Hon <jehon11@gmail.com>
2019-06-27 15:14:15 +0900

    include action controller mime responds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
   before_action :authenticate_request, except: :fallback_index_html
   attr_reader :current_employee
 
