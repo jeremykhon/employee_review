c5fa94b0164905a554ded88764cd1dec50505031
Jeremy Hon <jehon11@gmail.com>
2019-06-27 16:50:49 +0900

    clean up navbar code

--- client/src/components/navigation_bar.js
+++ client/src/components/navigation_bar.js
@@ -1,38 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const NavigationBar = ({ employee, onLogOut }) => {
   const signOut = () => {
     localStorage.removeItem('Authorization');
     onLogOut();
-  };
-
-  const renderIfLoggedIn = () => {
-    if (employee) {
-      return (
-        <div className="navbar-links-container">
-          {employee.is_admin
-            ? (
-              <div className="navbar-left">
-                <NavLink className="navbar-link" to="/admin">Admin view</NavLink>
-                <NavLink className="navbar-link" to="/employee">Employee view</NavLink>
-              </div>
-            )
-            : <div>Employee dashboard</div>
-          }
-          <div className="navbar-right">
-            <div className="navbar-name">
-              {`${employee.first_name} ${employee.last_name}`}
-            </div>
-            <div className="navbar-link" onClick={signOut}>
-              Sign out
-            </div>
-          </div>
-        </div>
-      );
-    }
-    return null;
   };
 
   if (employee) {
